e5f122cd4ea463c3e16caabaf51b238ffb56a2bd
Marcin <mkorycki93@gmail.com>
2017-10-14 16:56:23 +0200

    Basic configuration of Firebase added with example of usages.

--- app/src/main/java/project/bookrental/AddBookActivity.java
+++ app/src/main/java/project/bookrental/AddBookActivity.java
@@ -2,6 +2,13 @@ package project.bookrental;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 /**
@@ -14,5 +21,25 @@ public class AddBookActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); // TODO: add separate views for every Activity
+        workOnDatabase();
+    }
+
+    void workOnDatabase() {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                Log.d("MESSAGE:", value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w("MESSAGE:", databaseError.toException());
+            }
+        });
+        Log.d("BEFORE MESSAGE","zapisujemy!");
+        myRef.setValue("Hello, World!");
     }
 };
