2331412a77c44478d0a8d8265667841fe779a8b6
Marcin <mkorycki93@gmail.com>
2017-10-14 15:22:35 +0200

    Added onClick listeners for buttons.

--- app/src/main/java/project/bookrental/MainActivity.java
+++ app/src/main/java/project/bookrental/MainActivity.java
@@ -1,7 +1,10 @@
 package project.bookrental;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addOnClickListenersOnButtons();
+    }
+
+    private void addOnClickListenersOnButtons() {
+        Integer[] buttonNames = new Integer[] {R.id.addBookButton,R.id.removeBookButton, R.id.borrowBookButton, R.id.returnBookButton, R.id.aboutButton, R.id.exitButton};
+        for (int i = 0; i < buttonNames.length; i++) {
+            Button button = (Button)findViewById(buttonNames[i]);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    switch(v.getId()){
+                        case R.id.addBookButton:
+                            startActivity(new Intent(MainActivity.this, AddBookActivity.class));
+                            break;
+                        case R.id.removeBookButton:
+                            startActivity(new Intent(MainActivity.this, RemoveBookActivity.class));
+                            break;
+                        case R.id.borrowBookButton:
+                            startActivity(new Intent(MainActivity.this, BorrowBookActivity.class));
+                            break;
+                        case R.id.returnBookButton:
+                            startActivity(new Intent(MainActivity.this, ReturnBookActivity.class));
+                            break;
+                        case R.id.aboutButton:
+                            startActivity(new Intent(MainActivity.this, AboutActivity.class));
+                            break;
+                        case R.id.exitButton:
+                            finishAffinity();
+                    }
+                }
+            });
+        }
     }
 }
---
+++ app/src/main/java/project/bookrental/ReturnBookActivity.java
@@ -0,0 +1,18 @@
+package project.bookrental;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+/**
+ * Created by marcin on 14.10.17.
+ */
+
+public class ReturnBookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); // TODO: add separate views for every Activity
+    }
+};
