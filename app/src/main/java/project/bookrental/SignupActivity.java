87604a5d6a568a397747e1055c6bf4c8294520e5
Marcin <mkorycki93@gmail.com>
2017-10-28 14:33:14 +0200

    Fix error which automatically log user into application after successful finished register action.

--- app/src/main/java/project/bookrental/SignupActivity.java
+++ app/src/main/java/project/bookrental/SignupActivity.java
@@ -93,7 +93,6 @@ public class SignupActivity extends AppCompatActivity {
                                     Toast.makeText(SignupActivity.this, "Authentication failed." + task.getException(),
                                             Toast.LENGTH_SHORT).show();
                                 } else {
-                                    startActivity(new Intent(SignupActivity.this, MainActivity.class));
                                     finish();
                                 }
                             }
