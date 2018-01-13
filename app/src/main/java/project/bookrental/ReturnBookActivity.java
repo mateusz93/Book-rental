934a69d18fe655e3c0365ed10a1d694788d8ba85
Mateusz Wieczorek <wskmateusz@gmail.com>
2018-01-13 14:08:26 +0100

    Init return book activity

--- app/src/main/java/project/bookrental/ReturnBookActivity.java
+++ app/src/main/java/project/bookrental/ReturnBookActivity.java
@@ -13,6 +13,6 @@ public class ReturnBookActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); // TODO: add separate views for every Activity
+        setContentView(R.layout.activity_return_book);
     }
-};
+}
